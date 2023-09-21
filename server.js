05e0b50660f74ff433aefef209797957f095aa20
Thalys001 <thalyssouza708@gmail.com>
2023-09-20 21:51:43 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+// import { createServer } from 'node:http';
+
+// const server = createServer((request, response) => {
+//   response.write('oi')
+
+//   return response.end()
+// })
+
+// server.listen(3333)
+
+import { fastify } from 'fastify';
+
+const server = fastify()
+
+server.get('/', () => {
+  return 'Hello World'
+});
+
+server.get('/node', () => {
+  return 'Hello Node.js'
+});
+
+server.listen({
+  port: 3333,
+})
