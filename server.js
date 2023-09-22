f1c235fb255e543a989b21e66115b066b588f951
Thalys001 <thalyssouza708@gmail.com>
2023-09-21 23:52:06 -0300

    feat: creating routes

--- server.js
+++ server.js
@@ -9,14 +9,41 @@
 // server.listen(3333)
 
 import { fastify } from 'fastify';
+import { DatabaseMemory } from './database-memory.js'
 
 const server = fastify()
 
-server.get('/', () => {
-  return 'Hello World'
+const database = new DatabaseMemory()
+
+// POST http://localhost:3333/videos
+// PUT http://localhost:3333/videos/5
+
+server.post('/videos', (request, reply) => {
+  const { title, description, duration } = request.body
+
+  database.create({
+    title,
+    description,
+    duration,
+  })
+
+  console.log(database.list());
+
+  return reply.status(201).send()
 });
 
-server.get('/node', () => {
+server.get('/videos', () => {
+  const videos = database.list()
+
+  return videos
+});
+
+
+server.put('/videos/:id', () => {
+  return 'Hello Node.js'
+});
+
+server.delete('/videos/:id', () => {
   return 'Hello Node.js'
 });
 
